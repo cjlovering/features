3657ae72380cb62afbd2ea835b03163f6d5ebcfc
cjlovering <cjlovering@wpi.edu>
2020-08-13 14:36:08 -0400

    update datasets job params

--- datasets.sh
+++ datasets.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --time=00:45:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=16G
 #SBATCH -J job
 #SBATCH -o ./out/%j-0.out
 #SBATCH -e ./err/%j-0.out
-#SBATCH -a 0-15
+#SBATCH -a 0-3
 
 module load python/3.7.4 gcc/8.3
 . /gpfs/runtime/opt/anaconda/3-5.2.0/etc/profile.d/conda.sh
