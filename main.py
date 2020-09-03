094e7cfa0721302d729dee7f5989935bb3ae3528
cjlovering <cjlovering@wpi.edu>
2020-09-03 10:48:07 -0400

    update all t5

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def main(
 
     # Lower the following to (1, 0.1, 0.1) to speed up debugging.
     num_epochs = 50
-    limit_train_batches = 0.1
+    limit_train_batches = 1.0
     limit_test_batches = 1.0
 
     # Check 10% of the validation data every 1/10 epoch.
